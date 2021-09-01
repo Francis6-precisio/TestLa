3e260408cfe71c2213b1b643ac77ff99c171bfea
Francis  ABODO <abodojustin@gmail.com>
2021-09-01 11:03:10 +0100

    Button component is done

--- src/components/molecules/carItem/index.js
+++ src/components/molecules/carItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
+import Buttons from "../../atomes/buttons";
 import styles from "./styles";
 
 export default function CarItem(props) {
@@ -9,10 +10,26 @@ export default function CarItem(props) {
         style={styles.image}
         source={require("../../../assets/images/ModelX.jpeg")}
       />
+
       <View style={styles.titles}>
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <Buttons
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Custom Order was pressed");
+        }}
+      />
+      <Buttons
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("Existing Inventory was pressed");
+        }}
+      />
     </View>
   );
 }
